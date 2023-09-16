1272b15d21fb48e9eede9252ccb8f3a469798ed1
David Olivero <daniolidavid32@gmail.com>
2023-09-16 14:36:44 -0500

    TypeScript files added

--- POO-&_TypeScript/JS/01-jsonClass.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () => {
-    let bike = {
-        color: "red",
-        model: "sedan",
-        maxSpeed: "60km/h"
-    };
-
-    console.log(bike)
-})
---
+++ POO-&_TypeScript/scripts/Ts/02-hellloWorld.ts
@@ -0,0 +1,2 @@
+console.log("Hello, world!")
+alert("Hola desde TypeScript, mensaje editado")
