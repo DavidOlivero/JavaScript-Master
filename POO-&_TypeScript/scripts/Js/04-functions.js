febe1159b8a65b8f87dd153b2ce944ada37b5c3e
David Olivero <daniolidavid32@gmail.com>
2023-09-18 20:15:16 -0500

    Files for TypeScript class

---
+++ POO-&_TypeScript/scripts/Js/04-functions.js
@@ -0,0 +1,5 @@
+"use strict";
+function addedNumbers(number1, number2) {
+    return `El resultado de ${number1} y ${number2} es ${number1 + number2}`;
+}
+console.log(addedNumbers(23, 4));
---
+++ POO-&_TypeScript/scripts/Ts/04-functions.ts
@@ -0,0 +1,5 @@
+function addedNumbers(number1: number, number2: number): string {
+    return `El resultado de ${number1} y ${number2} es ${number1 + number2}`
+}
+
+console.log(addedNumbers(23, 4))
