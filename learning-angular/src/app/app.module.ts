3f44ee3b23088b76bbcd54476a705b1d7d99ea23
David Olivero <daniolidavid32@gmail.com>
2023-09-19 20:32:26 -0500

    Settings files changed

--- learning-angular/src/app/app.module.ts
+++ learning-angular/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/home/app.component';
+import { VideoGamesComponent } from './components/video-games/video-games.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoGamesComponent
   ],
   imports: [
     BrowserModule
