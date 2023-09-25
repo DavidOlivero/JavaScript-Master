4d0a406189d1a714325e78a1a3e4fc3884e9b1ce
David Olivero <daniolidavid32@gmail.com>
2023-09-24 19:15:30 -0500

    Templates

--- learning-angular/src/app/components/home/home.component.ts
+++ learning-angular/src/app/components/home/home.component.ts
@@ -9,13 +9,19 @@ import { settings } from "../../models/settings";
 export class HomeComponent {
   public title: string
   public description: string
+  public isLogin: boolean
 
   constructor () {
     this.title = settings.title[0]
     this.description = settings.description[0]
+    this.isLogin = false
   }
 
   public changeTitle = ():void => {
     this.title = 'Página de títulos cambiada'
+  }
+
+  public loginSystem = (): void => {
+    this.isLogin ? this.isLogin = false : this.isLogin = true
   }
 }
