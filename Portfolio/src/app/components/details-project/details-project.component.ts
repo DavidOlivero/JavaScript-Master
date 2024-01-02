a19eeb324a12064e67218f464ca90075da92b508
David Olivero <daniolidavid32@gmail.com>
2024-01-02 14:26:35 -0500

    Details for each project added

---
+++ Portfolio/src/app/components/details-project/details-project.component.ts
@@ -0,0 +1,52 @@
+import { APP_ID, Component } from '@angular/core';
+import { OnInit } from "@angular/core";
+import { Router, ActivatedRoute, Params, Route } from "@angular/router";
+import { ProjectService } from "../../services/project.service";
+import { Project } from "../../models/project";
+import { API } from "../../configs/global";
+
+@Component({
+  selector: 'app-details-project',
+  templateUrl: './details-project.component.html',
+  styleUrls: ['./details-project.component.css'],
+  providers: [ProjectService]
+})
+export class DetailsProjectComponent implements OnInit {
+  public url: string
+  public project: Project
+
+  constructor(
+    private _projectService: ProjectService,
+    private _router: Router,
+    private _route: ActivatedRoute
+    ) {
+    this.url = API
+    this.project = new Project()
+  }
+
+  public ngOnInit(): void {
+    this._route.params.subscribe({
+      next: (params) => {
+        const id = params['id'];
+
+        this.getProject(id)
+      },
+
+      error: (error) => {
+          console.log(error)
+      }
+    })
+  }
+
+  private getProject(id: string): void {
+    this._projectService.getProject(id).subscribe({
+      next: (response) => {
+        this.project = response.project
+      },
+
+      error: (err) => {
+        console.log(err)
+      }
+    })
+  }
+}
